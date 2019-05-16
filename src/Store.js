8bf5e5f40844713f66ddcaf837a795d72154450f
blaisebakundukize <blaisebakundukize@gmail.com>
2019-05-16 17:55:29 +0200

    Chore(add pre-hooks): add git pre-hooks
    - ansure we cann't commit or push with eslint's errors and failing tests
    
    [Delivers #166056015]

--- src/Store.js
+++ src/Store.js
@@ -1,15 +1,15 @@
-//@redux store
-import {createStore, applyMiddleware} from "redux";
-import {composeWithDevTools} from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducers from "./reducers/index";
+// @redux store
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducers from './reducers/index';
 
-//@middleware
-const middleware=[thunk];
-const initialState={};
-//@creating store
-const Store= createStore(rootReducers,initialState,composeWithDevTools(applyMiddleware(...middleware)));
+// @middleware
+const middleware = [thunk];
+const initialState = {};
+// @creating store
+const Store = createStore(
+  rootReducers, initialState, composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default Store;
-
-
